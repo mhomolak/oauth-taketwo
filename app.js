46ad8b6c7b9e10f72a902035f3ef9927a11f2a6c
Mike Homolak <mhomolak@yahoo.com>
2016-04-03 13:52:59 -0700

    and another

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.get('/auth/twitter/callback',
 passport.use(new TwitterStrategy({
     consumerKey: process.env.TWITTER_CONSUMER_KEY,
     consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-    callbackURL: "http://127.0.0.1:3000/auth/twitter/callback"
+    callbackURL: process.env.HOST + "/auth/twitter/callback"
   },
   function(token, tokenSecret, profile, cb) {
     User.findOrCreate({ twitterId: profile.id }, function (err, user) {
